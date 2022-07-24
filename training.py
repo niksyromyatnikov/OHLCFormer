57165484ed2f022fcf35a40bdccdff4ae9f9471c
niksyromyatnikov <nik@serafima.ai>
2022-07-25 02:14:10 +0300

    Implemented a high-level interface for model training.
    Signed-off-by: niksyromyatnikov <nik@serafima.ai>

---
+++ training.py
@@ -0,0 +1,59 @@
+from pathlib import Path
+from dotmap import DotMap
+from models import ModelForFM
+from utils import load_model
+
+
+def run_training(model: ModelForFM = None,
+                 configs: DotMap = None,
+                 configs_path: str = None,
+                 model_dir: str = None,
+                 datasets: dict = None,
+                 evaluate: bool = False,
+                 verbose: bool = True,
+                 ) -> dict:
+    eval_result = {}
+
+    for name, dataset in datasets.items():
+        train_configs = {}
+
+        model = model if model is not None else load_model(configs, configs_path, model_dir)
+
+        if model is None:
+            raise ValueError('Expected one of [model object, configs, configs_path, model_dir] to be provided')
+
+        configs = model.get_configs()
+
+        if isinstance(dataset, str):
+            train_configs['dataset_path'] = dataset
+            dataset = train_configs
+
+        if isinstance(dataset, dict):
+            for config_name, config_val in dataset.items():
+                try:
+                    model.set_config(config_name, config_val)
+                except TypeError as e:
+                    print(f'Failed to set config {config_name} to {config_val}')
+                    print(e)
+
+        model.load_dataset()
+
+        trainer_configs = model.get_configs().get('trainer', {})
+
+        if model_dir is not None:
+            trainer_configs['default_root_dir'] = model_dir
+        elif trainer_configs.get('default_root_dir', None) is None:
+            raise ValueError('Expected model_dir or trainer.default_root_dir to be provided')
+
+        trainer_configs['default_root_dir'] = Path(trainer_configs['default_root_dir']) / name
+
+        trainer = model.configure_trainer(**trainer_configs)
+        trainer.fit(model)
+
+        if evaluate:
+            eval_result[name] = trainer.test(model, verbose=verbose)
+
+        model = None
+
+    if evaluate:
+        return eval_result
