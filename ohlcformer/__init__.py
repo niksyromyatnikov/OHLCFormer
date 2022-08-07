f94a3da862265ee45fec8c8b0b713faee4efbb3e
niksyromyatnikov <nik@serafima.ai>
2022-08-07 15:56:58 +0300

    Fixed flake8 "imported but unused (F401)".
    Signed-off-by: niksyromyatnikov <nik@serafima.ai>

--- ohlcformer/__init__.py
+++ ohlcformer/__init__.py
@@ -1,3 +1,7 @@
+# flake8: noqa
+# There's no way to ignore "F401 '...' imported but unused" warnings in this
+# module, but to preserve other warnings. So, don't check this module at all.
+
 from .utils import (
     load_model,
     load_model_configs,
